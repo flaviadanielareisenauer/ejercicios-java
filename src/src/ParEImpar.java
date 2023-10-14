8c09b3d11c87665559e61c0885c2038adc084ec3
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-10-13 22:18:57 -0300

    par e impar

---
+++ src/src/ParEImpar.java
@@ -0,0 +1,16 @@
+package src;
+
+public class ParEImpar {
+
+    public static void main(String[] args) {
+        int num;
+        num = 12;
+        
+        while (num >0 -2)
+            num = num-2;
+        if (num == 0) 
+            System.out.print("Es par");
+        else 
+            System.out.print("Es impar");       
+    }
+}
